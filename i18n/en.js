4749caef1159c26061f8c1e7c5a2f00350d98923
MainVirtualizer <nbdd0121@gmail.com>
2015-08-06 21:21:01 +0800

    Start doing i18n

---
+++ i18n/en.js
@@ -0,0 +1,85 @@
+var i18n = {
+	game: {
+		profile: "Profile",
+
+		sexual_mouth: "Erogenous Zone (Mouth)",
+		sexual_throat: "Erogenous Zone (Throat)",
+		sexual_nipple: "Erogenous Zone (Nipple)",
+		sexual_curi: "Erogenous Zone (Clitoris)",
+
+		feature: {
+			energeticSmile: 'Energetic Smile',
+			cheerfulCharm: 'Cheerful Charm',
+			graceful: 'Graceful',
+			pure: 'Pure',
+			angelic: 'Angelic',
+			charming: 'Charming',
+			matureAppeal: 'Mature Appeal',
+			devilishCharm: 'Devilish Charm',
+			dignified: 'Dignified',
+			noble: '高贵',
+			bashful: '害羞',
+			meek: '温顺',
+			bewitchingCharm: '诱人魅力',
+			elegant: '上品',
+			refined: '优雅',
+			completelyDevoted: '完全服从',
+			servant: '仆从',
+			secretPast: '过去的秘密',
+			fatigue: '疲劳',
+			pastIndiscretion: '过去的过错',
+		},
+		propensity: {
+			licentious: "淫乱",
+			m: "M女",
+			anal: "喜欢后穴",
+			twohole: "喜欢两穴",
+			hentai: "变态",
+			service: "喜欢奉仕",
+			creampie: "喜欢中出",
+			drink: "喜欢饮精",
+		}
+	},
+
+	createurl: "Create Data URL",
+	invalidsave: "Invalid CM3D2 Save",
+	invalidjson: "Invalid JSON",
+
+	bodyEditor: "Body Editor",
+	featureEditor: "Feature Editor",
+	propensityEditor: "Propensity Editor",
+	pleaseSelect: "Please Select",
+
+	utility: "Utilities",
+
+	ui: {
+		load: "Load",
+		save: "Save",
+		loadjson: "Load JSON",
+		savejson: "Save JSON",
+
+		close: "Close",
+
+		editBody: "Edit Body",
+		editFeature: "Edit Feature",
+		editPropensity: "Edit Propensity",
+	},
+
+	util: {
+		unlockBodyLimits: "解锁全部女仆身高限制",
+		unlockBodyLimitsFinished: "女仆身高限制已全部解锁",
+		allItems: "解锁全部物品",
+		allItemsFinished: "全部物品已解锁",
+		allTrophies: "解锁全部奖杯",
+		allTrophiesFinished: "全部奖杯已解锁",
+		maidClassMax: "当前女仆的女仆称号升至满级",
+		maidClassMaxFinished: "当前女仆的女仆称号已全部升至满级",
+		yotogiClassMax: "当前女仆的夜伽称号升至满级",
+		yotogiClassMaxFinished: "当前女仆的夜伽称号已全部升至满级",
+		allSkills: "当前女仆全部技能满级",
+		allSkillsFinished: "当前女仆的技能已全部解锁并升至满级",
+		allWork: "当前女仆全部工作满级",
+		allWorkFinished: "当前女仆的工作已升至满级"
+	}
+
+};
